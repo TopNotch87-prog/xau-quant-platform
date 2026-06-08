4103d79acc15e9eb054d25bbbbdf1ebca143c47a
TopNotch87-prog <wizmerlin1@gmail.com>
2026-06-08 18:42:44 +0300

    Update strategy_agent.py

--- strategy_agent.py
+++ strategy_agent.py
@@ -1,1 +1,16 @@
+class StrategyAgent:
 
+    def choose(self, regime):
+
+        mapping = {
+
+            "TREND_UP": "trend_following",
+
+            "TREND_DOWN": "trend_following",
+
+            "RANGE": "mean_reversion",
+
+            "HIGH_VOL": "breakout"
+        }
+
+        return mapping[regime]
