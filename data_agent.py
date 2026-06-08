1b7b36594a624526528ae3c853e4a3e699e42a8c
TopNotch87-prog <wizmerlin1@gmail.com>
2026-06-08 18:41:44 +0300

    Update data_agent.py

--- data_agent.py
+++ data_agent.py
@@ -1,1 +1,16 @@
+import yfinance as yf
+import pandas as pd
 
+class DataAgent:
+
+    def download(self):
+
+        data = yf.download(
+            "GC=F",
+            period="5y",
+            interval="1h"
+        )
+
+        data.to_csv("data/market_data.csv")
+
+        return data
