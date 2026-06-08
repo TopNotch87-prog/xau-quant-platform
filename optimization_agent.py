087209be464bcf671024e66dc4d473925f272769
TopNotch87-prog <wizmerlin1@gmail.com>
2026-06-08 18:43:14 +0300

    Update optimization_agent.py

--- optimization_agent.py
+++ optimization_agent.py
@@ -1,1 +1,28 @@
+from itertools import product
 
+class WalkForwardOptimizer:
+
+    def optimize(self, train_df):
+
+        best_sharpe = -999
+
+        best_params = None
+
+        for fast, slow in product(
+                range(10,50,5),
+                range(50,200,10)
+        ):
+
+            sharpe = self.test_strategy(
+                train_df,
+                fast,
+                slow
+            )
+
+            if sharpe > best_sharpe:
+
+                best_sharpe = sharpe
+
+                best_params = (fast, slow)
+
+        return best_params
