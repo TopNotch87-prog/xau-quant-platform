7e6fce97b46613c321f2badfa9abeb762c33cf9f
TopNotch87-prog <wizmerlin1@gmail.com>
2026-06-09 23:01:23 +0300

    Fix: Add get_config() function to config.py to resolve ImportError

--- config.py
+++ config.py
@@ -1,3 +1,14 @@
-INITIAL_CAPITAL=10000
-RISK_PER_TRADE=0.01
-SYMBOL='GC=F'
+INITIAL_CAPITAL = 10000
+RISK_PER_TRADE = 0.01
+SYMBOL = 'GC=F'
+
+
+def get_config():
+    """
+    Returns the configuration dictionary for the XAU Quant Platform
+    """
+    return {
+        'initial_capital': INITIAL_CAPITAL,
+        'risk_per_trade': RISK_PER_TRADE,
+        'symbol': SYMBOL
+    }
